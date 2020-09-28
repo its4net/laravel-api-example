9b2d3bfb195b4e8d2f78d45875bc51c568a558cb
Kris Scheppe <kris@scheppe.us>
2020-09-27 20:19:18 -0400

    Cleaned up the UserController and updated documentation.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,26 +17,6 @@ class UserController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created user in the database.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -59,43 +39,14 @@ class UserController extends Controller
         return response()->json($user,201);
     }
 
-    
+    /**
+     * Show user information
+     * 
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
     public function show(Request $request)
     {
         return response()->json($request->user(),200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
     }
 }
